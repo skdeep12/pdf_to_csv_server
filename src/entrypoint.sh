7134bac12dbbbfed29a9d566c0d415778bab9e9f
Samota, Subhash <subhash.samota@tesco.com>
2020-04-16 01:37:42 +0530

    reconciliation commit

---
+++ src/entrypoint.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+
+if [ "$#" = 0 ]
+then
+    python3.7 -m pip freeze
+fi
+
+postgres_ready() {
+python3.7 << END
+from sys import exit
+from psycopg2 import connect, OperationalError
+try:
+    connect(
+        dbname="$POSTGRES_DB",
+        user="$POSTGRES_USER",
+        password="$POSTGRES_PASSWORD",
+        host="postgres",
+    )
+except OperationalError as error:
+    print(error)
+    exit(-1)
+exit(0)
+END
+}
+cd csv_server
+if [ "$#" = 0 ]
+then
+    >&2 echo "No command detected; running default commands"
+    >&2 echo "Running migrations"
+    python3.7 manage.py migrate --noinput
+    >&2 echo "\n\nStarting development server: 127.0.0.1:8000\n\n"
+    python3.7 manage.py runserver 0.0.0.0:8000
+else
+    >&2 echo "Command detected; running command"
+    exec "$@"
+fi
